8445d05386d30137daa944a8e8f979b98186d7fa
Angelo Umali <carions46@gmail.com>
2020-09-12 13:30:24 +0800

    change status response

--- src/api/Income/income.controller.js
+++ src/api/Income/income.controller.js
@@ -4,12 +4,12 @@ const Income = require('../../model')['Income'];
 exports.getAllIncome = async (req, res) => {
     Income.find((err, income) => {
         if (err) {
-            res.json({
+            res.status(400).json({
                 status: "error",
                 message: err,
             });
         }
-        res.json({
+        res.status(200).json({
             status: "success",
             message: "Contacts retrieved successfully",
             data: income
